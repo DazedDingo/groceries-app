7213726e6dddf08841994824cd48bb1704553bdd
dazeddingo <zachbirney@gmail.com>
2026-04-12 18:24:20 +0100

    chore: fix tsconfig, jest config, firebase.json predeploy, untrack lib/

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
+  passWithNoTests: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
 };
